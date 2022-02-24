78d0385ed93e9210ebaf64d005e5f36a5ef1f4d0
karyssa-dandrea <karyssadandrea@gmail.com>
2022-02-24 11:54:13 -0800

    created useform hook and having a hard time with my auth

--- src/views/Auth/Auth.js
+++ src/views/Auth/Auth.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import UserForm from '../../components/UserForm/UserForm';
+import { useUser } from '../../context/UserContext';
+import { signInUser, signUpUser } from '../../services/users';
 
-export default function Auth() {
-  return <div>Auth</div>;
+export default function Auth({ isSigningUp = false }) {
+  const history = useHistory();
+  const { setUser } = useUser();
+
+  // const handleLogin = async (email, password) => {
+  //   try {
+  //     if (isSigningUp) {
+  //       await signUpUser(email, password);
+  //       history.replace('/confirm-email');
+  //     } else {
+  //       const resp = await signInUser(email, password);
+  //       setUser({ id: resp.id, email: resp.email });
+  //     }
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // };
+
+  // return <UserForm onSubmit={handleLogin} label={isSigningUp ? 'Sign Up' : 'Sign In'} />;
 }
